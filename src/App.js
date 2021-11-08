c1f6250e466729209a695089f16de45026cb4635
Anna Slobodian <anna.slobodian@mediamonks.com>
2021-11-08 22:50:50 +0200

    learn states

--- src/App.js
+++ src/App.js
@@ -31,13 +31,44 @@ function Btn() {
   return <button>{logged ? 'Enter' : text}</button>;
 }
 
-function WhoAmI ({name, surname, link, age}) {
-  return(
-    <div>
-      <h1>My name is {name.firstName} , age - {age()} surname - {surname} </h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+// function WhoAmI ({name, surname, link, age}) {
+//   return(
+//     <div>
+//       <h1>My name is {name.firstName} , age - {age()} surname - {surname} </h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   )
+// }
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: 'some text'
+    };
+  }
+
+  nextYear = () => {
+    console.log('++');
+    this.setState(state => ({
+      years: state.years + 1,
+      text: 'new text'
+    }))
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name} surname - {surname}, age - {this.state.years}
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
 }
 
 function App() {
@@ -45,15 +76,14 @@ function App() {
     <div className="App">
       <StrictMode>
         <Header />
-        <WhoAmI name={{firstName: 'John'}} age={()=> {return '28'}}  surname = "Smith" link="google.com" />
-        <WhoAmI name={{firstName: 'Alex'}} age={()=> {return '28'}} surname = "Alex" link="google2.com" />
+        <WhoAmI name="John" surname="Smith" link="google.com" />
+        <WhoAmI name="Alex" surname="Alex" link="google2.com" />
       </StrictMode>
       <Field />
       <Btn />
     </div>
   );
 }
-
 
 export { Header };
 export default App;
