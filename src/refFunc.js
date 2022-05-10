cdb388ba55370b9b506f77d486ea71a44c275c81
Anna Slobodian <anna.slobodian@mediamonks.com>
2022-05-11 00:54:26 +0300

    learn custom hooks

--- src/refFunc.js
+++ src/refFunc.js
@@ -2,13 +2,32 @@ import { useRef, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-const Form = () => {
-  const [text, setText] = useState('');
-  const myRef = useRef(1); // обновление юзреф не вызывает перерендеринг компонента
+function useUnputWithValidate(initialValue) {
+  const [value, setValue] = useState(initialValue);
 
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const validateInput = () => {
+    return value.search(/\d/) >= 0;
+  };
+
+  return { value, onChange, validateInput }; //  {value: value, onChange: onChange}
+}
+
+const Form = () => {
+  // const [text, setText] = useState('');
+  const myRef = useRef(1); // обновление юзреф не вызывает перерендеринг компонента
+  //const [textArea, setTextArea] = useState('');
+
+  const input = useUnputWithValidate('');
+  const textArea = useUnputWithValidate('');
   // const focusFirstTI = () => {
   //   myRef.current.focus();
   // };
+
+  const color = input.validateInput() ? 'text-danger' : null;
 
   useEffect(() => {
     myRef.current++;
@@ -19,13 +38,20 @@ const Form = () => {
     <Container>
       <form className="w-50 border mt-5 p-3 m-auto">
         <div className="mb-3">
+          <input
+            value={`${input.value} / ${textArea.value}`}
+            type="text"
+            className="form-control"
+            readOnly
+          />
           <label htmlFor="exampleFormControlInput1" className="form-label">
             Email address
           </label>
           <input
-            onChange={(e) => setText(e.target.value)}
+            onChange={input.onChange}
             type="email"
-            className="form-control"
+            value={input.value}
+            className={`form-control ${color}`}
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           />
@@ -36,6 +62,8 @@ const Form = () => {
           </label>
           <textarea
             // onClick={focusFirstTI}
+            onChange={textArea.onChange}
+            value={textArea.value}
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
