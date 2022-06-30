d29975fa077b000f392136f1636e144173bc3d59
Anna Slobodian <anna.slobodian@mediamonks.com>
2022-06-30 11:20:25 +0300

    learn NOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './useReducer';
+import App from './NOC';
 import { Button } from './App';
 import styled from 'styled-components';
 
